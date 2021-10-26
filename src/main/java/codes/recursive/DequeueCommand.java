f083b9c1004006e1985321d3a9e17c4ae97a2c84
recursivecodes <toddraymondsharp@gmail.com>
2021-10-26 10:23:33 -0400

    fix build

--- src/main/java/codes/recursive/DequeueCommand.java
+++ src/main/java/codes/recursive/DequeueCommand.java
@@ -1,15 +1,10 @@
 package codes.recursive;
 
-import codes.recursive.queue.AqConsumer;
-import io.micronaut.context.annotation.Property;
 import io.micronaut.context.annotation.Requires;
-import io.micronaut.context.annotation.Value;
 import io.micronaut.core.annotation.Introspected;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
-
-import javax.inject.Inject;
 
 @CommandLine.Command(name = "dequeue", description = "Dequeues messages from AQ (until interrupted with CTRL+C)")
 @Requires(property = "consumer.enabled")
